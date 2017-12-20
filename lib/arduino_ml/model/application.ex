0558a14cd9a53a70b971669f9d49310696b012b5
Antoine AUBÉ <aube.antoine@protonmail.com>
2017-12-20 23:13:02 +0100

    Added documentation

--- lib/arduino_ml/model/application.ex
+++ lib/arduino_ml/model/application.ex
@@ -7,6 +7,9 @@ defmodule ArduinoML.Application do
     transitions: [],
     initial: nil
 
+  @doc """
+  Returns the label of the initial state of the application.
+  """
   def initial(app) when is_map(app) do
     app.initial || app.states[0].label
   end
